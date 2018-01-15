2fb65df540f5494bda91c371b88394abf44dd988
Chigozie <ahamchigoziecollins@gmail.com>
2018-01-15 12:13:08 -0500

    python codes
    + video setup

---
+++ detector.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+recognizer = cv2.createLBPHFaceRecognizer()
+recognizer.load('recognizer/trainer.yml')
+cascadePath = "/home/pi/VideoCam/haarcascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+
+cam = cv2.VideoCapture(0)
+font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1)
+while True:
+    ret, im =cam.read()
+    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces=faceCascade.detectMultiScale(gray, 1.2,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+        Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+        if(Id==1):
+            Id="Chigozie"
+        elif(Id==2):
+            Id="Lawrence"
+        else:
+            Id="Unknown"
+        cv2.cv.PutText(cv2.cv.fromarray(im),str(Id), (x,y+h),font, 255)
+    cv2.imshow('im',im) 
+    if cv2.waitKey(100) & 0xFF==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
---
+++ newdatasetter.py
@@ -0,0 +1,32 @@
+import cv2
+
+detector=cv2.CascadeClassifier('/home/pi/VideoCam/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('/home/pi/VideoCam/haarcascades/haarcascade_eye.xml')
+cam = cv2.VideoCapture(0)
+
+Id=raw_input('enter your id: ')
+sampleNum=0
+while(True):
+    ret, img1 = cam.read()
+    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        #cv2.rectangle(img1,(x,y),(x+w,y+h),(255,0,0),2)
+        gray_face = cv2.resize((gray[y: y+h, x: x+w]), (110, 110))
+        eyes = eye_cascade.detectMultiScale(gray_face)
+        
+        for (ex, ey, ew, eh) in eyes:
+            #incrementing sample number 
+            sampleNum=sampleNum+1
+            #saving the captured face in the dataset folder
+            cv2.imwrite("/home/pi/VideoCam/dataSet/User."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+
+    cv2.imshow('frame1',img1)
+    #wait for 100 miliseconds 
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+    # break if the sample number is morethan 20
+    elif sampleNum>20:
+        break
+cam.release()
+cv2.destroyAllWindows()
